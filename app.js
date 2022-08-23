822e9e31afd8c3da48292b9911c1fa945e97d0de
ad3rinto <aad3rinto@gmail.com>
2022-08-23 21:51:23 +0100

    configured about and contact routes

--- app.js
+++ app.js
@@ -11,10 +11,18 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
+
 app.get("/", function(req, res){
   res.render("home", {homeContent: homeStartingContent})
 })
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent})
+})
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent})
+})
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
